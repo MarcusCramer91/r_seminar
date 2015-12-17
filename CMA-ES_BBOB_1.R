bb02afbd2ae866e20774b9409143bf0ce631b8d7
Marcus Cramer <marcus_cramer@t-online.de>
2015-12-17 17:32:57 +0100

    lalala

--- CMA-ES_BBOB_1.R
+++ CMA-ES_BBOB_1.R
@@ -1,21 +1,35 @@
-optimizer = function(par, fun, lower, upper, max_eval) {
-    #N = number of dimensions
-    #lambda = 4 + floor(3*log(N)): for two dimensions: 6, for 20 dimensions: 12
-    #max.iter = 100 * N^2
-    #mu = floor(lambda / 2): for two dimensions: 3, for 20 dimensions: 6
-    cma_es(par = par, fn = fun, lower = lower, upper = upper, control = list(maxit = max_eval))
+optimizer = function(par, fun, lower, upper, max_eval, stopFitness) {
+  #N = number of dimensions
+  #lambda = 4 + floor(3*log(N)): for two dimensions: 6, for 20 dimensions: 12
+  #max.iter = 100 * N^2
+  #mu = floor(lambda / 2): for two dimensions: 3, for 20 dimensions: 6
+  mu = 10
+  lambda = 10
+  maxit = max_eval / mu
+  if (is.null(stopFitness)) cma_es(par = par, fn = fun, lower = lower, upper = upper, 
+                                   control = list(mu = mu, lambda = lambda, maxit = maxit))
+  else cma_es(par = par, fn = fun, lower = lower, upper = upper, 
+              control = list(mu = mu, lambda = lambda, stopfitness = stopFitness))
+}
+
+optimizer_default = function(par, fun, lower, upper, max_eval, stopFitness) {
+  cma_es(par = par, fn = fun, lower = lower, upper = upper)
 }
 
 budget = 1000
-bbo_benchmark(optimizer, "cmaes", "cmaes_results", budget = budget)
+bbo_benchmark(optimizer, "cmaes", "cmaes_test01", budget = 100, dimensions = 2, instances = 1)
+bbo_benchmark_custom(optimizer, "cmaes", "cmaes_test01", budget = 100, dimensions = 2, instances = 1, 
+                     function_ids = 1, stopFitness = 0.005)
+bbo_benchmark_custom(optimizer_default, "cmaes", "cmaes_test_default_settings", budget = 100)
 
 #some explanations regarding the function
 #budget limits per iteration, meaning that if we have a population of 10 and a budget of 10, at max 100 function evaluations are conducted
 #function IDs corresponds to the functions to be used to assess the algorithm, 1-24 being non-noisy and 101-124 being noisy
 #replications probably means complete reruns of the whole function
 #instances - no idea
-bbo_benchmark_custom = function (optimizer, algorithm_id, data_directory, dimensions = c(2, 3, 5, 10, 20, 40), instances = c(1:5, 41:50)
-                                 , replications = 1L, budget = 1e+07, noisy = FALSE, function_ids = NULL) {
+bbo_benchmark_custom = function (optimizer, algorithm_id, data_directory, dimensions = c(2, 3, 5, 10, 20, 40), 
+                                 instances = c(1:5, 41:50), replications = 1L, 
+                                 budget = NULL, noisy = FALSE, function_ids = NULL, stopFitness = NULL) {
   if (is.null(function_ids)) {
     function_ids <- if (noisy) 
       101:124
@@ -32,22 +46,32 @@ bbo_benchmark_custom = function (optimizer, algorithm_id, data_directory, dimens
         for (run in 1:replications) {
           bbob_setup_experiment(algorithm_id, data_directory, 
                                 function_id, instance_id, dimension)
-          while (bbob_n_evaluations() < budget && bbob_optimality_gap() > 1e-08) {
+          #while loop not wanted
+          #while (bbob_n_evaluations() < budget && bbob_optimality_gap() > 1e-08) { 
             if (bbob_n_evaluations() > 0) 
               bbob_log_restart("independent restart")
             par <- runif(dimension, -4, 4)
+            
+            firstEval = bbob_f_eval(par) #first evaluation, get optim value
+            optimGap = bbob_optimality_gap()
+            optimValue = firstEval - optimGap
+            print(firstEval)
+            print(optimGap)
+            print(optimValue)
+            print(optimValue + stopFitness)
+            
             optimizer(par, bbob_f_eval, lower = rep(-5, dimension), upper = rep(5, dimension), 
-                      budget - bbob_n_evaluations())
+                      budget - bbob_n_evaluations(), optimValue + stopFitness)
           }
           bbob_end_experiment()
-        }
+        #}
       }
     }
   }
 }
 
 
-bbo_benchmark_custom(optimizer, "cmaes", "cmaes_test", budget = 10, function_ids = 24)
+bbo_benchmark_custom(optimizer, "cmaes", "cmaes_test", budget = 10, function_ids = 24, dimensions = 1)
 
 #internal stopping criterion only works if budget <= 100 * Dimension^2 * population
 optimizer2 = function(par, fun, lower, upper, max_eval) {
@@ -57,12 +81,12 @@ optimizer2 = function(par, fun, lower, upper, max_eval) {
 
 bbo_benchmark_custom(optimizer2, "cmaes", "cmaes_test", budget = 10, function_ids = 1)
 
-optimizer3 = function(par, fun, lower, upper, max_eval) {
+optimizer3 = function(dimension, function_id, instance_id, par, fun, lower, upper, max_eval) {
   #requires manual initialization of the functions
   #Sphere function
-  fun = makeSphereFunction(2)
+  fun = makeBBOBFunction(dimension, function_id, instance_id)
   cmaes(objective.fun = fun, start.point = par)
 }
 
 
-bbo_benchmark_custom(optimizer3, "cmaes", "cmaes_test", budget = 10, function_ids = 24, dimension = 2, instances = 1)
+bbo_benchmark_custom2(optimizer3, "cmaes", "cmaes_test", budget = 10, function_ids = 24, dimension = 2, instances = 1)
--- load_libraries.R
+++ load_libraries.R
@@ -1,57 +1,56 @@
-loadRelevantLibraries = function () {
-  if (!"BBmisc" %in% rownames(installed.packages())) install.packages("BBmisc")
-  if (!"stringr" %in% rownames(installed.packages())) install.packages("stringr")
-  if (!"smoof" %in% rownames(installed.packages())) install.packages("smoof")
-  if (!"devtools" %in% rownames(installed.packages())) install.packages("devtools")
-  require(BBmisc)
-  require(stringr)
-  require(smoof)
-  require(devtools)
+if (!"BBmisc" %in% rownames(installed.packages())) install.packages("BBmisc")
+if (!"stringr" %in% rownames(installed.packages())) install.packages("stringr")
+if (!"smoof" %in% rownames(installed.packages())) install.packages("smoof")
+if (!"devtools" %in% rownames(installed.packages())) install.packages("devtools")
+require(BBmisc)
+require(stringr)
+require(smoof)
+require(devtools)
+
+
+#get BBOB
+if (!"bbob" %in% rownames(installed.packages())) {
+  fn = file.path(tempdir(), "bbob_current.tar.gz")
+  URL = "http://coco.lri.fr/downloads/download15.03/bbobr.tar.gz"
+  download.file(URL, destfile=fn)
+  #ATTENTION: requires Rtools for windows
+  install.packages(fn, repos=NULL)
+  tryCatch(install.packages(fn, repost = NULL), 
+    stop(simpleError("automatic installation done according to the bbob documentation. However, it sometimes does not work. 
+           Manual download of just the R files and then manual installation via install.packages(filepath) does the trick"))) 
   
-  
-  #get BBOB
-  if (!"bbob" %in% rownames(installed.packages())) {
-    fn = file.path(tempdir(), "bbob_current.tar.gz")
-    URL = "http://coco.lri.fr/downloads/download15.03/bbobr.tar.gz"
-    download.file(URL, destfile=fn)
-    #ATTENTION: requires Rtools for windows
-    install.packages(fn, repos=NULL)
-    tryCatch(install.packages(fn, repost = NULL), 
-      stop(simpleError("automatic installation done according to the bbob documentation. However, it sometimes does not work. 
-             Manual download of just the R files and then manual installation via install.packages(filepath) does the trick"))) 
-    
-    #above is from the official documentation but does not work for me, this does
-    #after manually downloading just the r file
-    #install.packages("bbobrpackage.15.03.tar.gz", repos = NULL)
-    file.remove(fn)
-  }
-  require(bbob)
-  
-  
-  #load CMA-ES based on R
-  if (!"cmaes" %in% rownames(installed.packages())) {
-    fn = file.path(tempdir(), "CMA-ES-R.tar.gz")
-    URL = "https://cran.r-project.org/src/contrib/cmaes_1.0-11.tar.gz"
-    download.file(URL, destfile = fn)
-    install.packages(fn, repos = NULL)
-    file.remove(fn)
-  }
-  require(cmaes)
-  
-  
-  #load CMA-ES based on Java
-  if (!"rCMA" %in% rownames(installed.packages())) {
-    fn = file.path(tempdir(), "CMA-ES-Java.tar.gz")
-    URL = "https://cran.r-project.org/src/contrib/rCMA_1.1.tar.gz"
-    download.file(URL, destfile = fn)
-    install.packages(fn, repos = NULL)
-    file.remove(fn)
-  }
-  require(rCMA)
-  
-  #get Bossek's CMA-ES
-  if (!"cmaesr" %in% rownames(installed.packages())) {
-    install_github(repo = "jakobbossek/cmaesr")
-  }
-  require(cmaesr)
+  #above is from the official documentation but does not work for me, this does
+  #after manually downloading just the r file
+  #install.packages("bbobrpackage.15.03.tar.gz", repos = NULL)
+  file.remove(fn)
 }
+require(bbob)
+
+
+#load CMA-ES based on R
+if (!"cmaes" %in% rownames(installed.packages())) {
+  fn = file.path(tempdir(), "CMA-ES-R.tar.gz")
+  URL = "https://cran.r-project.org/src/contrib/cmaes_1.0-11.tar.gz"
+  download.file(URL, destfile = fn)
+  install.packages(fn, repos = NULL)
+  file.remove(fn)
+}
+require(cmaes)
+
+
+#load CMA-ES based on Java
+if (!"rCMA" %in% rownames(installed.packages())) {
+  fn = file.path(tempdir(), "CMA-ES-Java.tar.gz")
+  URL = "https://cran.r-project.org/src/contrib/rCMA_1.1.tar.gz"
+  download.file(URL, destfile = fn)
+  install.packages(fn, repos = NULL)
+  file.remove(fn)
+}
+require(rCMA)
+
+#get Bossek's CMA-ES
+if (!"cmaesr" %in% rownames(installed.packages())) {
+  install_github(repo = "jakobbossek/cmaesr")
+}
+require(cmaesr)
+
